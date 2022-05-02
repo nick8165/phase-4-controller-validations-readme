09f0d73085209c59eb73036527d0cc5db9a9c857
Nicholas Freeman <nick_freeman65@yahoo.com>
2022-05-02 14:06:59 -0500

    commit tree b28a8b0becc087429ff87c0a6da8d52dc753d4e3

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,14 @@
 class Bird < ApplicationRecord
+
+    def create
+        bird = Bird.create!(bird_params)
+        render json: bird, status: :created
+      end
+
+      def update
+        bird = find_bird
+        bird.update!(bird_params)
+        render json: bird
+      end
+
 end
